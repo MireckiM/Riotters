899c7c83ac9f3e8551de0ef094c2c36bb6598126
Mirecki <marmir@st.amuedu.pl>
2022-12-12 19:23:55 +0100

    Summary0.5

--- src/components/Summary.vue
+++ src/components/Summary.vue
@@ -1,19 +1,23 @@
 <template>
-  <v-card class="rounded-lg grey lighten-4"  flat>
-    <v-list-item three-line>
-      <v-list-item-content>
-        <div class="text-overline mb-4">OVERLINE</div>
-        <v-list-item-title class="text-h5 mb-1"> Headline 5 </v-list-item-title>
-        <v-list-item-subtitle
-          >Greyhound divisely hello coldly fonwderfully</v-list-item-subtitle
-        >
-      </v-list-item-content>
-
-      <v-list-item-avatar tile size="80" color="grey"></v-list-item-avatar>
-    </v-list-item>
+  <v-card class="rounded-lg grey lighten-4" flat>
+    <v-card-title class="text-h6 mb-1">
+      Summary <v-spacer></v-spacer
+      ><v-icon color="grey darken-1">mdi-dots-horizontal</v-icon></v-card-title
+    >
 
     <v-card-actions>
-      <v-btn outlined rounded text> Button </v-btn>
+      <v-btn fab flat depressed class="mb-2 mt-10">
+        <v-icon small color="deep-purple accent-2" class=""
+          >mdi-circle-medium</v-icon
+        >
+      </v-btn>
+      <p class="mt-13 grey--text caption">This month</p>
+      <v-btn fab flat depressed class="mb-2 mt-10">
+        <v-icon small color="grey lighten-1" class=""
+          >mdi-circle-medium</v-icon
+        >
+      </v-btn>
+      <p class="mt-13 grey--text caption">Last month</p>
     </v-card-actions>
   </v-card>
 </template>
